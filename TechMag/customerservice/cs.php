14618d055cd9fe04ff49593cbb8cbbc985bb155a
tahani 96 <tahoo.1417@gmail.com>
2017-08-16 09:50:30 +0300

    t

--- TechMag/customerservice/cs.php
+++ TechMag/customerservice/cs.php
@@ -265,19 +265,19 @@ window.onclick = function(event) {
 			var techspan = document.getElementsByClassName("close")[0];
 
 			// When the user clicks the button, open the modal
-			btn.onclick = function() {
-			    modal.style.display = "block";
+			techbtn.onclick = function() {
+			    techmodal.style.display = "block";
 			}
 
 			// When the user clicks on <span> (x), close the modal
 			span.onclick = function() {
-			    modal.style.display = "none";
+			    techmodal.style.display = "none";
 			}
 
 			// When the user clicks anywhere outside of the modal, close it
 			window.onclick = function(event) {
 			    if (event.target == techmodal) {
-			        modal.style.display = "none";
+			        techmodal.style.display = "none";
 			    }
 			}
 			</script>
